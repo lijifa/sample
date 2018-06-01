49563d593f2b5fd329aed291b6ed55bcf6e8bdf6
lijifa <lijifa518@163.com>
2018-06-01 08:31:26 +0000

    test Controllers

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Route::get('/', 'StaticPagesController@home');
+	Route::get('/help', 'StaticPagesController@help');
+	Route::get('/about', 'StaticPagesController@about');
 });
